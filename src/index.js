a1bbabd8da8b1a5189555b67ad8f56bbf19f0b38
Mislaine Marinho de Souza <93009301+mislainemds@users.noreply.github.com>
2022-07-24 23:04:51 -0300

    fix: heroku port problem

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ require('dotenv')
 require('express');
 const app = require('./app')
 
-const port = process.env.APP_PORT || 3000
+const port = process.env.APP_PORT || process.env.PORT || 3000
 
 app.listen(port, () => console.log(`app is running in port ${port}`))
