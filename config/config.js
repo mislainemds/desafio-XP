f6554614b8c8b26f97dd1dadc09fab2794cd76ec
Mislaine Marinho de Souza <93009301+mislainemds@users.noreply.github.com>
2022-07-24 22:58:56 -0300

    fix: config production environments

--- config/config.js
+++ config/config.js
@@ -9,17 +9,17 @@ const options = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER || 'root',
+    "password": process.env.MYSQL_PASSWORD || '1234',
+    "database": process.env.MYSQL_DB_NAME,
+    "host": process.env.MYSQL_HOST || 'localhost',
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER || 'root',
+    "password": process.env.MYSQL_PASSWORD || '1234',
+    "database": process.env.MYSQL_DB_NAME,
+    "host": process.env.MYSQL_HOST || 'localhost',
     "dialect": "mysql"
   }
 }
